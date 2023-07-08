5ba5e0430bcec96016751a734f013992d9d2b1b9
Harish Kumar <harishstudio877@gmail.com>
2023-07-08 21:57:36 +0530

    new project blog application

---
+++ src/Components/Topheader/TopHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TopHeader = () => {
+    return (
+        <div>
+            <h1>home</h1>
+            <h1>about</h1>
+        </div>
+    )
+}
+
+export default TopHeader

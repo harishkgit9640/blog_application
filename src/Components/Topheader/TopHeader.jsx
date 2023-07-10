88f4a0e02a9290db5716da8ebf83e1525cac1280
Harish Kumar <harishstudio877@gmail.com>
2023-07-10 09:38:40 +0530

    working on top header

--- src/Components/Topheader/TopHeader.jsx
+++ src/Components/Topheader/TopHeader.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
-
+import './topheader.css'
 const TopHeader = () => {
     return (
         <div>
-            <h1>home</h1>
-            <h1>about</h1>
+            <div className="top-bar">
+                <div className="top-left">
+                    <i className=" topSocialIcon fa-brands fa-facebook"></i>
+                    <i className=" topSocialIcon fa-brands fa-instagram"></i>
+                    <i className=" topSocialIcon fa-brands fa-linkedin"></i>
+                    <i className=" topSocialIcon fa-brands fa-github"></i>
+                </div>
+                <div className="top-center">
+                    <div className="top-list">
+                        <li className="topListItem">Home</li>
+                        <li className="topListItem">About</li>
+                        <li className="topListItem">Contact</li>
+                        <li className="topListItem">Write</li>
+                        <li className="topListItem">Login</li>
+                    </div>
+                </div>
+                <div className="top-right">
+                    <img className="top-img" src="#" alt="top-img" />
+                    <i className=" searchIcon fa-solid fa-magnifying-glass"></i>
+                </div>
+            </div>
+
         </div>
     )
 }

e3b9a4a9d5ae1e36819efd5a8172d1fb803e21e4
Harish Kumar <harishstudio877@gmail.com>
2023-07-12 09:19:08 +0530

    single post page has complete

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.json';
 import TopHeader from './Components/Topheader/TopHeader'
-import Home from './Components/Home/Home';
+import Single from './Components/single/Single';
 function App() {
   return (
     <div>
       <TopHeader />
-      <Home />
+      <Single />
     </div>
   );
 }
---
+++ src/Components/singlepost/SinglePost.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './singlePost.css';
+
+const SinglePost = () => {
+    return (
+        <div className='singlePost'>
+            <div className="singlePostWrapper">
+                <img src="https://www.appliedart.com/assets/images/blog/blogging-SMB.png" alt="" className="singlePostImg" />
+                <h1 className="singlePostTitle">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit
+                    <div className="singleEdit">
+                        <i className=" singleIcon fa-solid fa-pen-to-square"></i>
+                        <i className=" singleIcon fa-solid fa-trash-can"></i>
+                    </div>
+                </h1>
+
+                <div className="singlePostInfo">
+                    <span className="singleAuthor">Author : <b>Harish</b></span>
+                    <span className="singleDate">1 hours ago</span>
+                </div>
+                <div className="singlePostDesc">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis consectetur molestias suscipit laudantium provident. Hic nostrum ipsa culpa aspernatur placeat! Tempore fugiat inventore dolore suscipit dignissimos hic voluptate numquam consequuntur amet, atque itaque consectetur aspernatur quo corporis. Explicabo iure facilis, rem sint sequi eius esse minus ullam debitis aliquid pariatur tempore, reiciendis nostrum quis voluptatum ad? Explicabo molestias amet a repellat eaque numquam quisquam suscipit adipisci cum veniam! Laudantium tenetur ipsum nihil voluptatem facere, quisquam facilis aliquam, ad iure modi itaque eius consequuntur adipisci provident ratione quae! In possimus consectetur nihil saepe. Veritatis quasi quo qui exercitationem nemo. Porro, reiciendis.
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil esse nulla, maiores voluptatum reiciendis, rerum, consequatur quae ut autem reprehenderit quam voluptate tempore cumque! Dignissimos, odio consequuntur. Laboriosam iusto exercitationem ad beatae dolore labore vitae at ducimus facilis, consequuntur inventore quia quam deserunt accusamus esse non aperiam aliquam consequatur laborum?
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SinglePost

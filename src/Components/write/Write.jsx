a9d71a9159924fee0d398a02338dc53435c36f35
Harish Kumar <harishstudio877@gmail.com>
2023-07-22 08:28:56 +0530

    post api implimation

--- src/Components/write/Write.jsx
+++ src/Components/write/Write.jsx
@@ -1,22 +1,65 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import './write.css';
+import axios from 'axios';
+import { Context } from '../../context/Context';
 
 const Write = () => {
+    const { user } = useContext(Context);
+    const [file, setFile] = useState(null);
+    const [data, setData] = useState({
+        username: user.username,
+        title: "",
+        desc: "",
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setData({ ...data, [name]: value });
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const newPost = { data };
+        console.log(JSON.stringify(newPost));
+
+        if (file) {
+            const fileData = new FormData();
+            const filename = Date.now() + file.name;
+            fileData.append("name", filename)
+            fileData.append("file", file)
+            newPost.photo = filename;
+            try {
+                console.log(fileData);
+                // const postRes = await axios.post(`/upload`, fileData)
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        try {
+            // const postRes = await axios.post(`/post`, newPost)
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
+
     return (
         <div className='write'>
-            <img src="https://img.freepik.com/premium-photo/social-media-blog-concept-futuristic-icon-design-graphics-hand-with-smartphone_102583-6104.jpg" alt="writeImg" className="writeImg" />
+            {file &&
+                <img src={URL.createObjectURL(file)} alt="writeImg" className="writeImg" />}
             <form className="writeForm">
                 <div className="writeFormGroup">
                     <label htmlFor="fileInput">
                         <i className='writeIcon fas fa-circle-plus'></i>
                     </label>
-                    <input type="file" name="file" id="fileInput" style={{ display: "none" }} />
-                    <input type="text" name="title" id="title" placeholder='Title ...' autoFocus={true} className='writeInput' />
+                    <input type="file" onChange={(e) => setFile(e.target.files[0])} id="fileInput" style={{ display: "none" }} />
+                    <input type="text" name="title" value={data.title} onChange={handleChange} id="title" placeholder='Title ...' autoFocus={true} className='writeInput' />
                 </div>
                 <div className="writeFormGroup">
-                    <textarea type="text" placeholder='Tell your story...' className='writeInput writeText' ></textarea>
+                    <textarea type="text" name="desc" value={data.desc} onChange={handleChange} placeholder='Tell your story...' className='writeInput writeText' ></textarea>
                 </div>
-                <button className='writeSubmit'>Publish</button>
+                <button className='writeSubmit' onClick={handleSubmit}>Publish</button>
             </form>
         </div>
     )
